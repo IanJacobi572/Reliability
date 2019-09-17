b3fe46e8ac6777b7bde4c91658d47c0256926797
IanJacobi572 <iwj0001@tigermail.auburn.edu>
2019-09-17 10:42:00 -0500

    Re-added download script for niagara

---
+++ Niagara/downloadati.py
@@ -0,0 +1,13 @@
+from Preprocessing import from_ftp
+from ftplib import FTP
+import os
+path = 'C:/Niagara'
+remote_path = '/atitest4/'
+ftp = FTP('ftp1.onerheem.com')
+data_path = 'C:/Niagara/FieldTest'
+subfolders = [f.path for f in os.scandir(data_path) if f.is_dir() ]    
+for sub in subfolders:
+	name = os.path.basename(os.path.normpath(sub))
+	start_str = "Niagara-" + name
+	dl = from_ftp(str_date_start = "2019-5-1",path=path, remote_path=remote_path, ftp = ftp, start_str = start_str, data_path = sub)
+	dl.ftp_sync()
