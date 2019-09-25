6aceb12d23eb351a7dad512b89092e073af0ac3e
Anes <anes.wahran@gmail.com>
2019-09-25 10:42:31 -0500

    Updated split on events

--- Niagara/splitOnEvents.py
+++ Niagara/splitOnEvents.py
@@ -13,40 +13,38 @@ def find_nearest(array, value):
 
 directory = r'F:\Data on BDATAPROD2\Anes Madani\Niagara\UEF_Niagara\Conversion_Result'
 resultPath = r'C:\Niagara\Preprocessing_UEF'
-event_names = ['Ending Purge', 'Cut OUT', 'Cut IN', 'Waiting for T', 'Starting Purge', 'Starting Draw', 'Draw Complete']
+events = ['Purge', 'Draw', 'Cut']
+startings = ['Starting Purge', 'Starting Draw', 'Cut IN']
+endings = ['Ending Purge', 'Draw Complete', 'Cut OUT']
+
 for filename in os.listdir(directory):
     if not filename.startswith("E"):
+        print(filename)
         #Load Channels and Events datasets
         df = pd.read_csv(os.path.join(directory,filename))
         dfEvent = pd.read_csv(os.path.join(directory,'E_'+filename))
+        #Create a new column for each event with the number of the event
+        for event, starting, ending in zip(events, startings, endings):
+            df[event]=0
+            #Indecies of event
+            starting_indeces = dfEvent.loc[dfEvent['Desc'].str.contains(starting)]
+            ending_indeces = dfEvent.loc[dfEvent['Desc'].str.contains(ending)]
+            for i, j in zip(range(0, len(starting_indeces)), range(0, len(ending_indeces))):
+                start_event_time = starting_indeces.iloc[i][1]
+                end_event_time = ending_indeces.iloc[j][1]
+                start_idx = find_nearest(df['TimeStamp (sec)'].values, start_event_time)
+                end_idx = find_nearest(df['TimeStamp (sec)'].values, end_event_time)
+                df.loc[start_idx:end_idx, event]=str(i+1)
+                
+        #Create a 'Waiting For T' column
+        df['Waiting for T']=0
+        indeces = dfEvent.loc[dfEvent['Desc'].str.contains('Waiting for T')]
+        for i in range(0, len(indeces)):
+            time = indeces.iloc[i][1] 
+            idx = find_nearest(df['TimeStamp (sec)'].values, time)
+            df.loc[idx, 'Waiting for T']=str(i+1)
         
-        #Create a new column that contains the Draw Number
-        df['Draw Number']=0
-        df['Desc'] = ''
-        df['Gals Drawn'] = 0
-        start_idxs = []
-        for event in event_names:
-            #indecies of event
-            specific_events = dfEvent.loc[dfEvent['Desc'].str.contains(event)]
-            #since we need the number, so that we can output the draw number
-            for i in range(0, len(specific_events)):
-                event_time = specific_events.iloc[i][1]
-                idx = find_nearest(df['TimeStamp (sec)'].values, event_time)
-                if(event == 'Starting Draw'):
-                    df.loc[idx, 'Desc'] = 'Starting Draw # ' + str(i+1)
-                    #uncomment if needed, just adds gals drawn to the output
-                    gals = re.findall("\d+\.\d+", specific_events.iloc[i][5])[-1]
-                    df.loc[idx, 'Gals Drawn'] = gals
-                    start_idxs.append(idx)
-                elif(event == 'Draw Complete'):
-                    df.loc[idx, 'Desc'] = event
-                    df.loc[start_idxs[i]:idx, 'Draw Number'] = i+1
-                else:
-                    df.loc[idx, 'Desc'] = event
-        #result file
         df.to_csv(os.path.join(resultPath,filename), index = False)
-       
+
     else:
         continue
-#Maybe need to start TimpeStamps from 0 for each or not
-#Note to self: Maybe need to optimize the code but it works just fine (for now)
