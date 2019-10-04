3416af4d4bb8dcce10dfef4fbc3337b56537d2b5
Ian Jacobi <ian.jacobi@Rheem.com>
2019-10-04 09:42:23 -0500

    tdms looks outside of subdirs now

--- Niagara/RelDataProcess.py
+++ Niagara/RelDataProcess.py
@@ -78,7 +78,7 @@ target_cycles = {
 	'1' : 14400,
 	'2' : 25100,
 	'3' : 14400,
-	'Low Flow': 61320
+	'Low Flow': 15631
 }
 diff_cycles = {
 	'G9' : 11360,
--- Niagara/tdms.py
+++ Niagara/tdms.py
@@ -1,15 +1,8 @@
 import pandas as pd 
 import os
 from nptdms import TdmsFile
-
-data_path = '//onerheem/whd-onerheemdfs/Data2 on WM1FSPROD02/PDP/PROJECT FOLDERS/TNK PROJECTS/Proj EC06619/R&D/Development/DOE test'
-
-subfolders = [f.path for f in os.scandir(data_path) if f.is_dir()]    
-
-for sub in subfolders:
-    with os.scandir(sub) as list_of_entries:
-        for entry in list_of_entries:
-            if(entry.name.endswith('.tdms')):
+def tdms_extract(entry):
+     if(entry.name.endswith('.tdms')):
                 tdms_file = TdmsFile(entry)
                 for group in tdms_file.groups():
                     data = tdms_file.object(group).as_dataframe()
@@ -18,3 +11,13 @@ for sub in subfolders:
                     if not os.path.exists('C:/Niagara/UEF/' + group):
                         os.makedirs('C:/Niagara/UEF/' + group)
                     data.to_csv('C:/Niagara/UEF/' +group + "/" + entry.name[:-5] +".csv")
+data_path = '//onerheem/whd-onerheemdfs/Data2 on WM1FSPROD02/PDP/PROJECT FOLDERS/TNK PROJECTS/Proj EC06619/R&D/Development/DOE test'
+files = [f for f in os.scandir(data_path) if f.name.endswith('tdms')]
+for file in files:
+    tdms_extract(file)
+subfolders = [f.path for f in os.scandir(data_path) if f.is_dir()]    
+
+for sub in subfolders:
+    with os.scandir(sub) as list_of_entries:
+        for entry in list_of_entries:
+           tdms_extract(entry)
