Cycles fix attempt for low flow units

NiagaraRel&Field/NiagaraRel.py
@@ -85,11 +85,12 @@ class Niagara_Reliability(pr.Preprocessing_Base):
 			split_df["Category"] = "Reliability"
 			split_df["Cycles"] = self.count_non_consec_flames(split_df)
 			if station == 'H9' or station == "H8":
-				cycles_df = split_df.loc[split_df.Cycles == 1].copy()
-
+				cycles_df = split_df.loc[split_df.Cycles == '1'].copy()
+				print(cycles_df)
 				cycles_df["Distance"] =  np.append(0, np.ediff1d(cycles_df.index))
 				split_df.Cycles = 0 
-				split_df["Cycles"] = len(split_df.loc[cycles_df.loc[(cycles_df.Distance < 50) == (cycles_df.Distance > 20)].index, "Cycles"])
+				print(len(cycles_df.loc[(cycles_df.Distance > 18)]))
+				split_df.Cycles.values[0] = len(cycles_df.loc[(cycles_df.Distance > 18)])
 			split_df["Week"] =  week
 			#print(split_df['Unit_Name'].values[1])
 			split_df["Delta_T"] = self.delta_t(self.temp_cols, split_df)