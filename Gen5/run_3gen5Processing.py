e837b70a71311ce876064404e286838c594da42d
Anes <anes.wahran@gmail.com>
2019-11-08 16:27:19 -0600

    Update

--- Gen5/run_3gen5Processing.py
+++ Gen5/run_3gen5Processing.py
@@ -2,22 +2,20 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import os
-import matplotlib.pyplot as plt
 from scipy.signal import argrelextrema
 import Gen5Info as Info
+from datetime import datetime
+#from scipy.signal import argrelextrema
 
 directory = r'C:\gen5\preprocessed'
 resultPath = r'C:\gen5\processed'
 
-print('DATA PROCESS STARTING')
+print('DATA PROCESS STARTED')
 
 for fileName in os.listdir(directory):
 
     #Load dataset
-    df = pd.read_csv(os.path.join(directory,fileName))   
-    
-    if 'F11' in fileName:
-        continue
+    df = pd.read_csv(os.path.join(directory,fileName))
     
     print(fileName)
     
@@ -26,114 +24,86 @@ for fileName in os.listdir(directory):
     key = df.loc[1, 'Station']
     key = Info.info.get(key)
     
-    #TEMPORARY: Since the actual cycles were counted manually so we add a temporary column that will contain the actual cycles per row instead of hardcoded
-    if key.getStation()=='C3':
-        df['Actual Cycles']=1662
-    elif key.getStation()=='C4':
-        df['Actual Cycles']=1715
-    elif key.getStation()=='C5':
-        df['Actual Cycles']=1717
-    elif key.getStation()=='C6':
-        df['Actual Cycles']=1715
-    elif key.getStation()=='C9':
-        df['Actual Cycles']=1541
-    elif key.getStation()=='C10':
-        df['Actual Cycles']=1702
-    elif key.getStation()=='C11':
-        df['Actual Cycles']=1562
-    elif key.getStation()=='F1':
-        df['Actual Cycles']=591
-    elif key.getStation()=='F2':
-        df['Actual Cycles']=591
-    elif key.getStation()=='F3':
-        df['Actual Cycles']=582
-    elif key.getStation()=='F5':
-        df['Actual Cycles']=445
-    elif key.getStation()=='F6':
-        df['Actual Cycles']=498
-    elif key.getStation()=='F7':
-        df['Actual Cycles']=591
-    elif key.getStation()=='F8':
-        df['Actual Cycles']=579
-    elif key.getStation()=='F10':
-        df['Actual Cycles']=578  
-    elif key.getStation()=='F11':
-        df['Actual Cycles']=None    
-    
     #Combine Station and UnitName columns in a new column
     df['Station/UnitName']=key.getStation() + '/' + key.getIcn()
     #Addd Heater Base Model column
     df['Heater Base Model']=key.getBaseModel()
-    #Add Operational Mode column
-    df['Operational Mode']=key.getOperationalMode()
-    #Add Cycles Target per time column
-    df['Target Cycles/Day']=key.getCycles()
-    df['Target Cycles/180 Days']=key.getCycles()*180
     
     #Add Cycling Option that has 3 different types
-    if key.getCycles()==21:
-        df['Cycling Option']='3 (21 Cycles/Day)'
-    elif key.getCycles()==12:
-        df['Cycling Option']='2 (12 Cycles/Day)'
-    elif key.getCycles()==144:
-        df['Cycling Option']='1 (144 Cycles/Day)'
+    if key.getCycles()==78.5:
+        df['Cycling Option']='3 (78.5 Cycles/Day)'
+    elif key.getCycles()==76.5:
+        df['Cycling Option']='2 (76.5 Cycles/Day)'
+    elif key.getCycles()==80:
+        df['Cycling Option']='1 (80 Cycles/Day)'
         
-    print('\nSorting by date')
-    #Convert Date to datetime and sort by date
-    df['Date'] = pd.to_datetime(df.Date)
-    df.sort_values('Date', inplace=True)
-    print('Sort by date complete!')
-    
-    firstWeek=pd.to_datetime(key.getStartDate()).isocalendar()[1]
-    #print('Start Date week: ', firstWeek)
-    firstWeekRecorded=df['Date'].iloc[0].isocalendar()[1]
-    #print('First week recorded: ', firstWeekRecorded-firstWeek+1)
-    lastWeek=df['Date'].iloc[-1].isocalendar()[1]
-    #print('Last week: ', lastWeek)
-    #print('Last week recorded: ', lastWeek-firstWeek+1)
-    
-    #Consider the first recorded date as start date if start date is after first recorded date
-    #if firstWeekRecorded<firstWeek:
-    #    firstWeek=firstWeekRecorded        
-    
-    df.loc[0, 'Start Week']=firstWeek
-    df.loc[0, 'Start Date']=key.getStartDate()
-    
-    numberOfWeeks=lastWeek-firstWeek+1
-    numberOfCycles=df['Actual Cycles'].iloc[0]
-    numberOfCyclesperDay=key.getCycles()
-    #Get the week number for each row and cycles per week
-    print('\nCalculating weeks')
-    temp=-99
-    for index, row in df.iterrows():
-        currentWeek=row['Date'].isocalendar()[1] - firstWeek + 1
-        if temp!=currentWeek:
-        #if temp!=currentWeek and currentWeek>=0:
-            temp=currentWeek
-            print('week:', temp, 'from', lastWeek-firstWeek+1)
-            df.loc[index, 'Week']=currentWeek
-            df.loc[index, 'Actual Cycles/Week']=(numberOfCycles/numberOfWeeks)*currentWeek
-            df.loc[index, 'Target Cycles/Week']=numberOfCyclesperDay*currentWeek*7 #number of cycles per day * week number * 7 days
-        else:
-            pass
-    print('Weeks calculation complete!')
-    
-    
-    
-    n = int(key.getCycles()*0.8) #since we're supposed to have a local min in each X rows, we use 80% to be safe from noise
-    
-    #Get the local minimas of the appropriate parameter (LOW or UP HEATER TEMPERATURE)
-    if max(df['LOHTRTMP'])-min(df['LOHTRTMP']) > max(df['UPHTRTMP'])-min(df['UPHTRTMP']):
-        df['min'] = df.iloc[argrelextrema(df.LOHTRTMP.values, np.less_equal, order=n)[0]]['LOHTRTMP']
+    print('\nSorting by date/time')
+    #Sort by date then by time
+    df['Comb'] = df['Date'] + ' ' + df['Time']
+    df['Comb'] = pd.to_datetime(df['Comb'], infer_datetime_format=True)
+    df['Time'] = pd.to_datetime(df.Time, infer_datetime_format=True)
+    df['Date'] = pd.to_datetime(df.Date, infer_datetime_format=True)
+    if len(df['Date'].unique())>0:
+        df.sort_values(by=['Date', 'Time'], inplace=True)
     else:
-        df['min'] = df.iloc[argrelextrema(df.UPHTRTMP.values, np.less_equal, order=n)[0]]['UPHTRTMP']
-      
-        
-    #Difference between target and actual cycles
-    df['Progress by cycle number']=df['Target Cycles/180 Days']-df['Actual Cycles']
-    df['Progress by percentage']=df['Actual Cycles']*100/df['Target Cycles/180 Days']
-
+        df.sort_values(by=['Time'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    print('Sort by date/time complete!')
     
+    df['Start Date'] = key.getTeamStartDate()
+    df['Switch Start Date'] = key.getChangedDate()
+
+    actual_start_date = datetime.strptime(key.getTeamStartDate(), '%Y-%m-%d')
+    NL_start_date = datetime.strptime(key.getStartDate(), '%Y-%m-%d')
+    changed_start_date = datetime.strptime(key.getChangedDate(), '%Y-%m-%d')
+    delta_days = (NL_start_date - actual_start_date).days
+    old_days = (changed_start_date - actual_start_date).days
+    delta_weeks = int(delta_days/7)
+        
+    change_index = df.index[df['Date'].dt.date == changed_start_date.date()][0]
+        
+    day0 = pd.Series(pd.to_datetime(df.loc[0, 'Date'])).dt.dayofyear[0]
+    df['Day'] = df['Date'].dt.dayofyear-day0+delta_days+1
+
+    week0 = pd.Series(pd.to_datetime(df.loc[0, 'Date'])).dt.weekofyear[0]
+    df['Week'] = df['Date'].dt.weekofyear-week0+delta_weeks+1
+    
+    #df['deltaT'] = df['Comb'].diff().dt.total_seconds()
+    df['deltaT'] = df['Comb'].groupby(df['Day']).diff().dt.total_seconds()
+    df['deltaT'].fillna(0, inplace=True)
+
+    df['Time'] = df['Time'].dt.time
+    df['Date'] = df['Date'].dt.date
+    
+    df['Compressor Running Time (sec)'] = np.where(df['COMP_RLY']=='On', df['deltaT'],0)
+    df['Compressor Non Running Time (sec)'] = np.where(df['COMP_RLY']=='Off', df['deltaT'],0)
+    df['Compressor Running Time (hrs)'] = df['Compressor Running Time (sec)']/3600
+    df['Compressor Non Running Time (hrs)'] = df['Compressor Non Running Time (sec)']/3600
+    
+    target = 3338
+    df['Target (hrs)/row'] = df['deltaT']/3600
+    df['Compressor On/Day'] = 7
+    df['Target CRT/Day'] = 7
+    df['Compressor Off/Day'] = 0
+    df['Compressor On/Day'].iloc[change_index:] = df['Compressor Running Time (hrs)'].iloc[change_index:].groupby(df['Day'].iloc[change_index:]).transform('sum')
+    df['Compressor Off/Day'].iloc[change_index:] = df['Compressor Non Running Time (hrs)'].iloc[change_index:].groupby(df['Day'].iloc[change_index:]).transform('sum')
+    df['Recorded CRT/Day'] = df['Target (hrs)/row'].groupby(df['Day']).transform('sum')
+    df['Target CRT/Day'].iloc[change_index:] = 24
+    df['Progress/Day'] = df['Compressor On/Day']/df['Target CRT/Day']
+    df['Completed (hrs)']=df['Compressor Running Time (hrs)'].iloc[change_index:].sum() + old_days*7
+    df['Not Completed (hrs)']=target-df['Completed (hrs)']
+    df['Target (hrs)']=df['Target (hrs)/row'].iloc[change_index:].sum() + old_days*7
+    df['Progress'] = df['Completed (hrs)']/target
+    
+    df['Target'] = target
+    days_left = df.loc[0, 'Not Completed (hrs)']/24
+    last_date = df['Date'].iloc[-1]
+    target_date = last_date + pd.DateOffset(days=days_left)
+    df['Target Date'] = target_date.to_pydatetime().date()
+    df['Years Reached'] = df.loc[0, 'Completed (hrs)']*12/target
+    
+    df.drop(['Target (hrs)/row', 'Comb'], axis=1, inplace=True)
+   
     df.to_csv(os.path.join(resultPath,fileName), index = False)    
     
     print('\n-------------------------------------------------------------------------------------------------------------------\n')
